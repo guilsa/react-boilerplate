83adffb1c5a58b9a75c9b5bc94427719b5412fee
Guilherme Sa <guilsa001@gmail.com>
2016-04-27 09:53:23 -0700

    Simplify import

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Router, hasHistory } from 'react-router'
-import { MainContainer } from '../containers'
+import { MainContainer } from 'containers'
 
 const routes = (
   <Router history={hasHistory}>
